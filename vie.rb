cfdd031dae42bd53680809c72df405dce60e7529
katsumi <32640219+katsumi143@users.noreply.github.com>
2024-02-28 22:55:34 +1000

    vie v0.0.4

--- vie.rb
+++ vie.rb
@@ -6,8 +6,8 @@ class Vie < Formula
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/hakusoda/VIE/releases/download/v0.0.3/vie_macos_arm64.tar.gz"
-        sha256 "492b6e7ef850851172d469995afc2ef065c4995f4ab84b29a14e3a123cf513c7"
+        url "https://github.com/hakusoda/VIE/releases/download/v0.0.4/vie_macos_arm64.tar.gz"
+        sha256 "f85ac48e7b5adf268b56dba422f19a9220f16ffc8ffe65cfbf641e207667373d"
   
         def install
           bin.install "vie"
