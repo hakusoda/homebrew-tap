1d51572c07b1c63937e504d81199390ace4c58b4
katsumi <32640219+katsumi143@users.noreply.github.com>
2024-02-28 21:57:33 +1000

    Create vie.rb

---
+++ vie.rb
@@ -0,0 +1,17 @@
+class VIE < Formula
+    desc "VIE"
+    homepage "https://github.com/hakusoda/VIE"
+    version "0.0.3"
+    license "AGPL-3.0"
+  
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/hakusoda/VIE/releases/download/v0.0.3/vie_macos_arm64.tar.gz"
+        sha256 "492b6e7ef850851172d469995afc2ef065c4995f4ab84b29a14e3a123cf513c7"
+  
+        def install
+          bin.install "vie"
+        end
+      end
+    end
+  end
