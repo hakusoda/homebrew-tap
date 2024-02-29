ae17bb6a38b3ac36eb10db2efe19d2ea93006bef
katsumi <32640219+katsumi143@users.noreply.github.com>
2024-02-29 20:37:16 +1000

    VIE v0.0.5

--- vie.rb
+++ vie.rb
@@ -1,13 +1,13 @@
 class Vie < Formula
     desc "VIE"
     homepage "https://github.com/hakusoda/VIE"
-    version "0.0.4"
+    version "0.0.5"
     license "AGPL-3.0"
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/hakusoda/VIE/releases/download/v0.0.4/vie_macos_arm64.tar.gz"
-        sha256 "f85ac48e7b5adf268b56dba422f19a9220f16ffc8ffe65cfbf641e207667373d"
+        url "https://github.com/hakusoda/VIE/releases/download/v0.0.5/vie_macos_arm64.tar.gz"
+        sha256 "79ffc38e9f325a1c94bf144d252f204634c4a70a07b461445d2e4de4a4a05dd8"
   
         def install
           bin.install "vie"
